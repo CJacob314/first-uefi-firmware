ed6ac63995a9fb7060c1d494d160970416bc2913
Jacob Cohen <jacob@jacobcohen.info>
2024-12-27 11:20:22 -0600

    Made `qemu.sh` actually runnable from any directory

--- qemu.sh
+++ qemu.sh
@@ -4,11 +4,11 @@ set -e
 DIR="$(dirname "${BASH_SOURCE[-1]}")"
 
 # Build
-cargo build --target x86_64-unknown-uefi
+cargo build --manifest-path="$DIR/Cargo.toml" --target x86_64-unknown-uefi
 
 # Make imitation UEFI system partition
-mkdir -p esp/efi/boot
-cp ./target/x86_64-unknown-uefi/debug/first-uefi-app.efi esp/efi/boot/bootx64.efi
+mkdir -p "$DIR/esp/efi/boot"
+cp "$DIR/target/x86_64-unknown-uefi/debug/first-uefi-app.efi" "$DIR/esp/efi/boot/bootx64.efi"
 
 # Copy over OVMF firmware (this step might be a bit finicky on other systems!)
 if [[ ! ( -f "$DIR/OVMF_CODE.4m.fd" && -f "$DIR/OVMF_VARS.4m.fd" ) ]]; then
@@ -17,6 +17,6 @@ fi
 
 # Run qemu
 qemu-system-x86_64 -enable-kvm \
-    -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.4m.fd \
-    -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.4m.fd \
-    -drive format=raw,file=fat:rw:esp
+    -drive if=pflash,format=raw,readonly=on,file="$DIR/OVMF_CODE.4m.fd" \
+    -drive if=pflash,format=raw,readonly=on,file="$DIR/OVMF_VARS.4m.fd" \
+    -drive format=raw,file="fat:rw:$DIR/esp"
